78563c16138f996a0d6fa5de8729f2c1aa06f576
mimyrtek <mimyrtek@gmail.com>
2026-01-08 13:28:56 +0000

    drop2

--- envs/dev/sandbox/backend.tf
+++ envs/dev/sandbox/backend.tf
@@ -2,18 +2,20 @@ terraform {
   backend "s3" {
     bucket = "tfstate"
     key    = "dev/sandbox/terraform.tfstate"
+    region = "us-east-1"
 
-    # MinIO (S3-compatible) in-cluster
-    endpoint = "http://minio-tfstate.tf-state.svc.cluster.local:9000"
-    region   = "us-east-1"
+    endpoints = {
+      s3 = "http://minio-tfstate.tf-state.svc.cluster.local:9000"
+    }
 
-    # MinIO/S3 compatibility flags
-    force_path_style            = true
+    # MinIO compatibility
+    force_path_style = true
+    insecure         = true
+
+    # Prevent AWS lookups (STS/IAM/metadata)
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-
-    # If you later add TLS in front of MinIO, flip this to false and use https://
-    insecure = true
   }
 }
